6dca5219ea5e589644d495d3b960a47ffe93f278
nirbhay-design <sharma.59@iitj.ac.in>
2026-06-12 22:18:44 +0530

    more evaluation strategies like top_k, min_p, and beam_search

--- src/eval_script/decoding.py
+++ src/eval_script/decoding.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 class Decoding():
     def __init__(self, model, vocab):
         self.model = model
@@ -24,6 +25,7 @@ class Decoding():
         return y_out
 
     def beam_search(self, x, max_len=30, beam_width=5):
+        all_probs = {}
         #keep top k logits for first step, and then keep top k sequences
         embedding, pos_encoding = self.model.model.get_embedding(x)
         device = embedding.device
@@ -31,13 +33,53 @@ class Decoding():
         y_out = torch.ones(batch_size, 1, device=device).type(torch.int64) # <SOS> token vector
         # for _ in range(max_len):
         decoder_output = self.model.model.get_decoding(embedding, pos_encoding, y_out)
-        _, predicted_words = torch.topk(decoder_output[:,-1:,:] , k=beam_width) # keep top k logits
-        print(predicted_words.shape)
-        return None
+        values, indicies = torch.topk(F.softmax(decoder_output[:,-1:,:], dim = -1), beam_width) # topk probs
+        beam = indicies # (B, 1, k)
+        probs = values
+        while y_out.shape[1] < max_len:
+            for i in range(beam_width):
+                curr_inp = torch.cat([y_out, beam[:,:,i]], dim=1) # (B, seq_len+1)
+                decoder_output = self.model.model.get_decoding(embedding, pos_encoding, curr_inp)
+                values, indicies = torch.topk(F.softmax(decoder_output[:,-1:,:], dim = -1), beam_width) # topk probs
+                curr_probs = probs[0,0,i]*values
+
+                all_probs[i+1] = {}
+                all_probs[i+1][beam[0,0,i].item()] = {curr_probs[0][0][j]: indicies[0][0][j].item() for j in range(beam_width)}
+
+            print(all_probs)
+            exit(0)
+            
+        
+        return y_out
+
+    def min_p(self, x, max_len=30, p = 0.7):
+        embedding, pos_encoding = self.model.model.get_embedding(x)
+        device = embedding.device
+        batch_size = embedding.shape[0]
+        y_out = torch.ones(batch_size, 1, device=device).type(torch.int64) # <SOS> token vector
+        for _ in range(max_len):
+            decoder_output = self.model.model.get_decoding(embedding, pos_encoding, y_out)
+            probs = F.softmax(decoder_output[:,-1:,:], dim = -1) # topk probs
+            threshold = p*probs.max()
+            mask = probs >= threshold
+            y = torch.ones_like(probs)*torch.tensor(-1000000.0)
+            alive = F.softmax(torch.where(probs[0,0,:]>=threshold.item(), probs[0,0,:], y[0,0,:]), dim=-1)
+            sampled_index = alive.multinomial(num_samples=1, replacement=True)
+            # print(sampled_index.unsqueeze(0).shape)
+            y_out = torch.cat([y_out, sampled_index.unsqueeze(0)], dim=1)
+        return y_out
         
 
-    def min_p(self, x, max_len=30, p = 0.9):
-        pass
-
     def top_k(self, x, max_len=30, k=5):
-        pass
+        embedding, pos_encoding = self.model.model.get_embedding(x)
+        device = embedding.device
+        batch_size = embedding.shape[0]
+        y_out = torch.ones(batch_size, 1, device=device).type(torch.int64)
+        for _ in range(max_len):
+            decoder_output = self.model.model.get_decoding(embedding, pos_encoding, y_out)
+            _, indicies = decoder_output[:,-1:,:].topk(k, dim = -1) # topk probs
+            sampled_index = indicies.flatten()[torch.randint(0, indicies.numel(), (1,))]
+            # print(sampled_index.unsqueeze(0).shape)
+            y_out = torch.cat([y_out, sampled_index.unsqueeze(0)], dim=1)
+
+        return y_out
